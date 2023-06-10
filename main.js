1b2b0372665101258072230076d2808d9ad72d3f
Ghassan <johndoe@example.com>
2023-06-10 21:32:56 +0300

    Tets12313

--- main.js
+++ main.js
@@ -4,18 +4,70 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { Sky } from 'three/addons/objects/Sky.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer({antialias:true});
-const controls = new OrbitControls( camera, renderer.domElement );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//physics
+const CdHuman = 1; // انسيابية جسم الإنسان
+const L = 8, W = 5;  // طول و عرض المظلة 
+const P0 = 101.325;  // قيمة الضغط عند سطح البحر
+const M = 0.0289644; // الكتلة المولية للهواء
+const R = 8.31;  // ثابت الغازات العام
+const T0 = 15;  // درجة الحرارة عند سطخ الارض بالسيليسويس 
+const humanWeight = 80;  // وزن الانسان   Variable
+const parachuteWeight = 5;  // وزن الباراشوت   Variable
+const g = 9.82;         //ثابت الجاذبية الأرضية  
+const A = L * W;        // مقطع السطح العرضي للباراشوت
+const AHuman = 1.4; // مقطع سطح العرضي للانسان 
+const Cd = 2; //انسيابية الباراشوت
+const Ci = 1; // قوة الرفع 
+let Ay
+let vwy = 10
+
+let h = 4500;          // الأرتفاع الإبتدائي
+let Vy = 0; //السرعة الإبتدائية على المحور العمودي
+let Vx = 20; // السرعة الابتدائية على المحور الافقي
+let rho, T, P; //درجة الحرارة و كثافة الهواء و الضغط الجوي 
+
+const h0 = h;
+
+const Fg = (m, g) => {
+  return m * g;
+}
+const FDrag = (Cd, A, rho, V) => {
+  return 0.5 * Cd * A * rho * V ** 2;
+}
+const FLift = (Ci, A, rho, V) => {
+  return 0.5 * Ci * A * rho * V ** 2;
+}
+
+const PCalculate = (h, T) => {
+  return P0 * Math.exp((-M * g * h) / (R * T));
+}
+
+const TCalculate = (h) => {
+  const temp = T0 - 0.0065 * h;
+  return temp + 273.15;
+}
+
+const rhoCalculate = (P, T) => {
+  return (P * M) / (R * T)*100;
+}
+
+const FWind = (Cd, A, rho, V) => {
+  return 0.5 * Cd * A * rho * V ** 2;
+}
 
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio( window.devicePixelRatio );
-document.body.appendChild( renderer.domElement );
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
 let sky = new Sky();
-  sky.scale.setScalar( 450000 );
-  scene.add( sky );
-let	sun = new THREE.Vector3();
+sky.scale.setScalar(450000);
+scene.add(sky);
+let sun = new THREE.Vector3();
 
 const effectController = {
   turbidity: 10,
@@ -28,101 +80,122 @@ const effectController = {
 };
 //stats for sky
 const uniforms = sky.material.uniforms;
-					uniforms[ 'turbidity' ].value = effectController.turbidity;
-					uniforms[ 'rayleigh' ].value = effectController.rayleigh;
-					uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
-					uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+uniforms['turbidity'].value = effectController.turbidity;
+uniforms['rayleigh'].value = effectController.rayleigh;
+uniforms['mieCoefficient'].value = effectController.mieCoefficient;
+uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
 
-          const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
-					const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+const theta = THREE.MathUtils.degToRad(effectController.azimuth);
 
-					sun.setFromSphericalCoords( 1, phi, theta );
+sun.setFromSphericalCoords(1, phi, theta);
 
-					uniforms[ 'sunPosition' ].value.copy( sun );
+uniforms['sunPosition'].value.copy(sun);
 
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 1, 0);
 
-let nigga,helicopter,parachute//variables for models
+let human, helicopter, parachute//variables for models
 const loader = new GLTFLoader();
 
 //loading models...
-loader.load('./helicopterModel/scene.gltf', function(gltf) {
-  helicopter=gltf.scene
-  helicopter.scale.set(0.4,0.4,0.4)
-  helicopter.position.y= 1.5
-  helicopter.position.x=16
+loader.load('./helicopterModel/scene.gltf', function (gltf) {
+  helicopter = gltf.scene
+  helicopter.scale.set(0.4, 0.4, 0.4)
+  helicopter.position.y = h0
+  helicopter.position.x = 16
   helicopter.rotation.z = 0.2
-scene.add(helicopter);
+  helicopter.scale.set(5, 5, 5)
+  scene.add(helicopter);
 
 });
-let cjnumber=0
 
-function jumb(){
-if(cjnumber ==0){
-  loader.load('./CjModel/scene.gltf', function(gltf) {
-    nigga=gltf.scene
-    nigga.position.x = helicopter?.position.x
-    nigga.position.y= -2.5
-    nigga.position.z= 4
-    
-        scene.add(nigga);
-     
-      })
-    cjnumber+=1;
-  }
-      else
-      console.log('cjalreadyexist')
+let cjnumber = 0
 
-}
+loader.load('./CjModel/scene.gltf', function (gltf) {
+  human = gltf.scene
+  human.position.y = h
+  human.position.z = 0
+  human.rotation.x = 90
+  human.scale.set(10, 10, 10)
+  scene.add(human);
+});
 
-function pJumb(){
-  if(nigga){
-  loader.load('./parachute/scene.gltf', function(gltf) {
-    parachute=gltf.scene;
-    parachute.position.x=nigga.position.x;
-    parachute.position.y= nigga.position.y + 0.5;
-    parachute.scale.set(0.1,0.1,0.1)
+// function jumb(){
+// if(cjnumber ==0){
+//   loader.load('./CjModel/scene.gltf', function(gltf) {
+//     human=gltf.scene
+//     human.position.x = helicopter?.position.x
+//     human.position.y= helicopter?.position.y-5
+//     human.position.z= 4
+
+//         scene.add(human);
+
+//       })
+
+//     cjnumber+=1;
+
+//   }
+//       else
+//       console.log('cjalreadyexist')
+
+// }
+
+let opened = false
+let push = false
+function pJumb() {
+  if (human) {
+
+    loader.load('./parachute/scene.gltf', function (gltf) {
+      parachute = gltf.scene;
+      parachute.position.x = human.position.x;
+      parachute.position.y = human.position.y + 7;
+      parachute.position.z = human.position.z - 32;
+      opened = true;
       scene.add(parachute);
-    })}
-    else{
-    loader.load('./CjModel/scene.gltf', function(gltf) {
-      nigga=gltf.scene
-      nigga.position.x = helicopter?.position.x
-      nigga.position.y= -2.5
-      nigga.position.z= 4
-      
-          scene.add(nigga);
     })
-    loader.load('./parachute/scene.gltf', function(gltf) {
-      parachute=gltf.scene;
-      parachute.position.x=nigga.position.x;
-      parachute.position.y= nigga.position.y + 0.5;
-      parachute.scale.set(0.1,0.1,0.1)
-        scene.add(parachute);
-      })
-    }
+  }
+
+  // else{
+  // loader.load('./CjModel/scene.gltf', function(gltf) {
+  //   human=gltf.scene
+  //   human.position.x = helicopter?.position.x
+  //   human.position.y= -2.5
+  //   human.position.z= 4
+
+  //       scene.add(human);
+  // })
+  // loader.load('./parachute/scene.gltf', function(gltf) {
+  //   parachute=gltf.scene;
+  //   parachute.position.x=human.position.x;
+  //   parachute.position.y= human.position.y + 0.5;
+  //   parachute.scale.set(0.1,0.1,0.1)
+  //     scene.add(parachute);
+  //   })
+  // }
 
 }
-  document.getElementById("btn").addEventListener("click", jumb);
-
-  document.getElementById("pbtn").addEventListener("click", pJumb);
 
 
+// document.getElementById("btn").addEventListener("click", jumb);
 
-  //ground
-var groundGeometry = new THREE.PlaneGeometry(300 , 300);
-const groundtext = new THREE.TextureLoader().load( 'atlas.png' );
+document.getElementById("pbtn").addEventListener("click", pJumb);
+
+
+
+//ground
+var groundGeometry = new THREE.PlaneGeometry(300, 300);
+const groundtext = new THREE.TextureLoader().load('atlas.png');
 groundtext.colorSpace = THREE.SRGBColorSpace;
 groundtext.magFilter = THREE.NearestFilter;
 groundtext.wrapS = THREE.RepeatWrapping;
 groundtext.wrapT = THREE.RepeatWrapping;
-groundtext.repeat.set(1, 1 );
-const ground = new THREE.Mesh( groundGeometry, new THREE.MeshLambertMaterial( { map: groundtext, side: THREE.DoubleSide } ) );
-scene.add( ground );
-ground.position.y = -40;
-ground.rotation.x = -Math.PI / 2; 
+groundtext.repeat.set(1, 1);
+const ground = new THREE.Mesh(groundGeometry, new THREE.MeshLambertMaterial({ map: groundtext, side: THREE.DoubleSide }));
+scene.add(ground);
+ground.position.y = 0;
+ground.rotation.x = -Math.PI / 2;
 
 
 scene.add(light);
@@ -134,43 +207,99 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize( window.innerWidth, window.innerHeight );
-animate()
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  animate()
 }
-window.addEventListener( 'resize', onWindowResize );
+window.addEventListener('resize', onWindowResize);
+camera.position.set(0, 10, 20);
 
+camera.position.z = 500;
+
+var clock = new THREE.Clock();
 
 function animate() {
-  //helicopter movment
-if (helicopter !== undefined) {
-  if(helicopter.position.x >0 ){
-    helicopter.position.x -= 0.01;
-}
-if(helicopter.position.x <2){
-  if(helicopter.rotation.z >0)
-    helicopter.rotation.z -= 0.001
- }
+  var deltaTime = clock.getDelta()
+
+  if (h > -12.5 && !opened) {
+    if (human !== undefined) {
+      camera.position.set(human.position.x, human.position.y + 75, human.position.z + 50);
+      camera.lookAt(human.position)
+      human.position.y = h
+    }
+    T = TCalculate(h);
+    P = PCalculate(h, T);
+
+    rho = rhoCalculate(P, T);
+    let FnetY = Fg(humanWeight + parachuteWeight, g)
+    Ay = FnetY / (humanWeight + parachuteWeight);
+    Vy += Ay * deltaTime;
+    let hs = (Vy * deltaTime);
+    h -= hs;
+
+    console.log(` 
+    height: ${h + 13} m 
+    speed: ${Vy} m/s 
+    Temp: ${T} K 
+    Pressure: ${P} Pa
+    Acceleration: ${Ay} m/s^2
+    DeltaTime : ${deltaTime}
+    Fnet : ${FnetY}
+    FG : ${Fg(humanWeight + parachuteWeight, g)}
+    rho : ${rho}
+
+    
+    `
+    )
+
+  } else if (h > -12.5) {
+
+    if (human !== undefined) {
+      camera.position.set(human.position.x, human.position.y + 75, human.position.z + 50);
+      camera.lookAt(human.position)
+      human.position.y = h
+    }
+    T = TCalculate(h);
+    P = PCalculate(h, T);
+
+    rho = rhoCalculate(P, T);
+    let FnetY = Fg(humanWeight + parachuteWeight, g) - FDrag(Cd, A, rho, Vy+vwy)
+    Ay = FnetY / (humanWeight + parachuteWeight);
+    Vy += Ay * deltaTime;
+    let hs = Vy * deltaTime;
+    h -= hs;
+
+    console.log(` 
+    height: ${h + 13} m 
+    speed: ${Vy} m/s 
+    Temp: ${T} K 
+    Pressure: ${P} Pa
+    Acceleration: ${Ay} m/s^2
+    DeltaTime : ${deltaTime}
+    Fnet : ${FnetY}
+    FG : ${Fg(humanWeight + parachuteWeight, g)}
+    Fdrag : ${FDrag(Cd, A, rho, Vy)}
+    rho : ${rho}
+
+
+    `)
+
+    human.rotation.x = 0
+    camera.position.set(human.position.x + 25, human.position.y, human.position.z - 50);
+    camera.lookAt(human.position)
   }
-  //
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+
+
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
   controls.update();
-  if (nigga) {
-    var modelPosition = new THREE.Vector3();
-    nigga.getWorldPosition(modelPosition);
-
-    // Adjust the camera position and look-at target as desired
-    camera.position.set(modelPosition.x, modelPosition.y , modelPosition.z + 5);
-    camera.lookAt(modelPosition);
-  }
-
 }
 animate();
 
-const ambientLight = new THREE.AmbientLight( 0xeeeeee );
-scene.add( ambientLight );
+const ambientLight = new THREE.AmbientLight(0xeeeeee);
+scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 4 );
-directionalLight.position.set( 1, 1, 0.5 ).normalize();
-scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+directionalLight.position.set(1, 1, 0.5).normalize();
+scene.add(directionalLight);
 
